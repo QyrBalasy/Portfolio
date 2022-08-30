ef5a6124538115992c0729fa2450517406b1cd59
qyrBalasy <shakarimsungat@gmail.com>
2022-08-31 00:44:19 +0600

    chore: add content

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React from 'react'
 
 const Home = () => {
   return (
-    <>
-    <div> hello blya!!</div>
-    </>
+    <div className="flex">
+      <div>
+        <h1 className="text-5xl">
+          Rayan Adlrdard
+          <span className='text-yellow-500'>Front-end </span>
+          Developer
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, volutpat feugiat placerat lobortis. Natoque
+          rutrum semper sed suspendisse nunc lectus.
+        </p>
+        <button>
+          HIRE ME
+          <span> arrow right</span>
+        </button>
+      </div>
+      <div>image is here</div>
+    </div>
   )
 }
 export default Home
