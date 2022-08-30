5d8b6ee4e48152bde177c5b64fae59118e157e31
qyrBalasy <shakarimsungat@gmail.com>
2022-08-31 00:45:25 +0600

    chore: regular edits

--- src/SiteContainer.tsx
+++ src/SiteContainer.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import MainRouter from './routes/MainRouter'
 import Aside from './views/aside/Aside'
-import Footer from './views/footer/Footer'
-import Header from './views/header/Header'
 import Sidebar from './views/sidebar/Sidebar'
 
 
 const SiteContainer = () => {
   return (
-    <div className="container max-w-7xl mx-auto flex justify-between">
-      <Aside />
-      <MainRouter />
-      <Sidebar />
+    <div className="container bg-[#18191A]">
+      <div className="max-w-7xl mx-auto flex justify-between">
+        <Aside />
+        <MainRouter />
+        <Sidebar />
+      </div>
     </div>
   )
 }
