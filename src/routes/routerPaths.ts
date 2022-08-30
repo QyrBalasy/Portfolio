cfaad44f48d9cc3932df1ad9acd790993034342c
qyrBalasy <shakarimsungat@gmail.com>
2022-08-31 00:42:53 +0600

    feat: add routerpath

---
+++ src/routes/routerPaths.ts
@@ -0,0 +1,23 @@
+export namespace PathsMap {
+  export const root = {
+    get: () => '/',
+  }
+  export const home = {
+    get: () => '/home',
+  }
+  export const services = {
+    get: () => '/service',
+  }
+  export const cv = {
+    get: () => '/cv',
+  }
+  export const portfolio = {
+    get: () => '/portfolio',
+  }
+  export const blog = {
+    get: () => '/blog',
+  }
+  export const contact = {
+    get: () => '/contacts'
+  }
+}
