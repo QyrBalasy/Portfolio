293f31da93a36a12918a06ff124c2bc2da475672
qyrBalasy <shakarimsungat@gmail.com>
2022-08-31 22:20:48 +0600

    fix: add layout colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      white: '#ffff',
+      silver: '#E4E6EB',
+      lightBg: '#F0F0F6',
+      paragraphFont: '#767676',
+      darkView1: '#242526',
+      darkView2: '#3A3B3C',
+      darkBg: '#18191A',
+      lightDark: '#2B2B2B',
+      yellow: '#FFB400',
+    },
   },
   plugins: [],
 };
