d5bd975f93b84795738437e112b55f328177f27c
qyrBalasy <shakarimsungat@gmail.com>
2022-08-31 22:21:17 +0600

    chore: add badge element

---
+++ src/components/UI/text.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Badge = styled.div`
+  display: inline-block;
+  padding: 6px 8px;
+  border-radius: 3px;
+  background-color: #587992;
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: 600;
+  color: #ffffff;
+`
