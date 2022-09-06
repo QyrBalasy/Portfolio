b1b1660f293286412d8e3e2cda1deed3b1587c9a
qyrBalasy <shakarimsungat@gmail.com>
2022-09-06 22:06:48 +0600

    fix: edit skills component add layout & other comp

--- src/components/Simple/aside/Skills.tsx
+++ src/components/Simple/aside/Skills.tsx
@@ -1,22 +1,85 @@
 import React from 'react'
 
-const Skills = () => {
+const SoftSkills = () => {
+  const kzLang = 100,
+    ruLang = 100
+  const engLang = 60
   return (
-    <div>
-      <h4>Languages</h4>
-        <div>
-          <p>Bangla</p>
-          <p>100%</p>
-        </div>
-        <div>
+    <div className='my-6'>
+      <ul>
+        <h4>Languages</h4>
+        <li className="grid grid-cols-2">
+          <p>Kazakh</p>
+          <p className="text-right">{kzLang}%</p>
+          <span className="col-span-2 h-2  w-full rounded border border-yellow bg-lightDark"></span>
+        </li>
+        <li className="grid grid-cols-2">
+          <p>Russian</p>
+          <p className="text-right">{ruLang}%</p>
+          <span className="col-span-2 h-2  w-full rounded border border-yellow bg-lightDark"></span>
+        </li>
+        <li className="grid grid-cols-2">
           <p>English</p>
-          <p>80%</p>
-        </div>
-        <div>
-          <p>Spanish</p>
-          <p>100%</p>
-        </div>
+          <p className="text-right">{engLang}%</p>
+          <span className="col-span-2 h-2  w-full rounded border border-yellow bg-lightDark"></span>
+        </li>
+      </ul>
     </div>
   )
 }
-export default Skills
+export default SoftSkills
+
+export const HardSkills = () => {
+  const kzLang = 100,
+    ruLang = 100
+  const engLang = 60
+  return (
+    <div className="my-6">
+      <ul>
+        <h4>HardSkills</h4>
+        <li className="grid grid-cols-2">
+          <p>Html</p>
+          <p className="text-right">{kzLang}%</p>
+          <span className="col-span-2 h-2  w-full rounded border border-yellow bg-lightDark"></span>
+        </li>
+        <li className="grid grid-cols-2">
+          <p>CSS</p>
+          <p className="text-right">{ruLang}%</p>
+          <span className="col-span-2 h-2  w-full rounded border border-yellow bg-lightDark"></span>
+        </li>
+        <li className="grid grid-cols-2">
+          <p>Js</p>
+          <p className="text-right">{engLang}%</p>
+          <span className="col-span-2 h-2  w-full rounded border border-yellow bg-lightDark"></span>
+        </li>
+        <li className="grid grid-cols-2">
+          <p>Php</p>
+          <p className="text-right">{engLang}%</p>
+          <span className="col-span-2 h-2  w-full rounded border border-yellow bg-lightDark"></span>
+        </li>
+        <li className="grid grid-cols-2">
+          <p>Wordpress</p>
+          <p className="text-right">{engLang}%</p>
+          <span className="col-span-2 h-2  w-full rounded border border-yellow bg-lightDark"></span>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export const ExtraSkills = () => {
+  const kzLang = 100,
+    ruLang = 100
+  const engLang = 60
+  return (
+    <div className="my-6">
+      <ul>
+        <h4>Extra Skills</h4>
+        <li>Bootstrap, Materialize</li>
+        <li>Stylus, Sass, Less</li>
+        <li>Gulp, Webpack, Grunt</li>
+        <li>GIT Knowledge</li>
+      </ul>
+    </div>
+  )
+}
