3d0c9d6288f94db289f73b8c41ec133a7e05ba18
qyrBalasy <shakarimsungat@gmail.com>
2022-09-06 22:07:47 +0600

    fix: add switch comp

--- src/views/sidebar/Sidebar.tsx
+++ src/views/sidebar/Sidebar.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ToggleSwitch } from '../../components/UI/switch'
 import { PathsMap } from '../../routes/routerPaths'
 
 const Sidebar = () => {
   return (
-    <nav className="flex w-24 flex-col bg-darkView1 justify-center">
-      <Link to={PathsMap.home.get()}>Home</Link>
-      <Link to={PathsMap.services.get()}>Service</Link>
-      <Link to={PathsMap.cv.get()}>Cv</Link>
-      <Link to={PathsMap.portfolio.get()}>Portfolio</Link>
-      <Link to={PathsMap.blog.get()}>Blog</Link>
-      <Link to={PathsMap.contact.get()}>Contact</Link>
-    </nav>
+    <div className="bg-darkView1 flex flex-col justify-center">
+      <ToggleSwitch />
+      <nav className="flex w-24 flex-col justify-center ">
+        <Link to={PathsMap.home.get()}>Home</Link>
+        <Link to={PathsMap.services.get()}>Service</Link>
+        <Link to={PathsMap.cv.get()}>Cv</Link>
+        <Link to={PathsMap.portfolio.get()}>Portfolio</Link>
+        <Link to={PathsMap.blog.get()}>Blog</Link>
+        <Link to={PathsMap.contact.get()}>Contact</Link>
+      </nav>
+    </div>
   )
 }
 export default Sidebar
