072aeb93d04fd333f9b5c079b8dd9c11e819fc1a
qyrBalasy <shakarimsungat@gmail.com>
2022-09-06 22:07:23 +0600

    feat: create button

---
+++ src/components/UI/buttons.tsx
@@ -0,0 +1,22 @@
+import styled, { css } from "styled-components";
+
+type ButtonProps = {
+  color?: 'primary' | 'secondary'
+  size?: 'default' | 'big'
+}
+
+export const Button = styled.button<ButtonProps>`
+  padding-top: 10px;
+  padding-bottom: 10px;
+  background-color: #ffb400;
+  width: 220px;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  justify-content: center;
+  ${props =>
+    props.color === 'primary' &&
+    css`
+      padding: 8px 32px;
+    `};
+`
