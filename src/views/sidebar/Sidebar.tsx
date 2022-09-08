f9de6db9ed6c2d5ebba61223aee40ffc26ff4df9
mavesna <sungat@mybuh.kz>
2022-09-08 18:02:49 +0600

    feat: mapped sidebar & add tooltip file

--- src/views/sidebar/Sidebar.tsx
+++ src/views/sidebar/Sidebar.tsx
@@ -1,19 +1,62 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { ToggleSwitch } from '../../components/UI/switch'
-import { PathsMap } from '../../routes/routerPaths'
+import React, {ReactNode} from 'react'
+import {NavLink} from 'react-router-dom'
+import {BriefcaseIcon, CodeIcon, CommentsIcon, GraduationCapIcon, HomeIcon, PenIcon} from '../../assets/icons/Icons'
+import {ToggleSwitch} from '../../components/UI/switch'
+import {PathsMap} from '../../routes/routerPaths'
+
+type SidebarDataT = {
+  title: string
+  path: string
+  icon: ReactNode
+}
 
 const Sidebar = () => {
+  const SidebarData: SidebarDataT[] = [
+    {
+      title: 'Home',
+      path: PathsMap.home.get(),
+      icon: <HomeIcon className="h-10 w-10 rounded-[50%] bg-yellow p-2" />,
+    },
+    {
+      title: 'Service',
+      path: PathsMap.services.get(),
+      icon: <CodeIcon className="h-10 w-10 rounded-[50%] bg-yellow p-2" />,
+    },
+    {
+      title: 'Cv',
+      path: PathsMap.cv.get(),
+      icon: <GraduationCapIcon className="h-10 w-10 rounded-[50%] bg-yellow p-2" />,
+    },
+    {
+      title: 'Portfolio',
+      path: PathsMap.portfolio.get(),
+      icon: <BriefcaseIcon className="h-10 w-10 rounded-[50%] bg-yellow p-2" />,
+    },
+    {
+      title: 'Blog',
+      path: PathsMap.home.get(),
+      icon: <PenIcon className="h-10 w-10 rounded-[50%] bg-yellow p-2" />,
+    },
+    {
+      title: 'Contact',
+      path: PathsMap.contact.get(),
+      icon: <CommentsIcon className="h-10 w-10 rounded-[50%] bg-yellow p-2" />,
+    },
+  ]
   return (
-    <div className="bg-darkView1 flex flex-col justify-center">
+    <div className="flex flex-col justify-center bg-darkView1">
       <ToggleSwitch />
-      <nav className="flex w-24 flex-col justify-center ">
-        <Link to={PathsMap.home.get()}>Home</Link>
-        <Link to={PathsMap.services.get()}>Service</Link>
-        <Link to={PathsMap.cv.get()}>Cv</Link>
-        <Link to={PathsMap.portfolio.get()}>Portfolio</Link>
-        <Link to={PathsMap.blog.get()}>Blog</Link>
-        <Link to={PathsMap.contact.get()}>Contact</Link>
+      <nav className="flex w-24 flex-col items-center justify-center gap-10">
+        {SidebarData.map((items, index) => {
+          return (
+            <ul key={index}>
+              <NavLink to={items.path}>
+                {items.icon}
+                {items.title}
+              </NavLink>
+            </ul>
+          )
+        })}
       </nav>
     </div>
   )
