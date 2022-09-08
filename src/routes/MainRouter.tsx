c2bd4d38cff94a2ebda5557cb2acec68ec448df4
qyrBalasy <shakarimsungat@gmail.com>
2022-09-08 21:20:06 +0600

    feat: add 404page

--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Error404 from "../pages/ErrorPage/Error404";
 import Blog from "./blog/Blog";
 import Contact from "./contact/Contact";
 import Cv from "./cv/Cv";
@@ -10,17 +11,18 @@ import { PathsMap } from "./routerPaths";
 
 const MainRouter = () => {
   return (
-    <>
+    <main>
       <Routes>
         <Route path={PathsMap.root.get()} element={<Home />} />
         <Route path={PathsMap.home.get()} element={<Home />} />
         <Route path={PathsMap.services.get()} element={<MyServices />} />
-        <Route path={PathsMap.cv.get()}element={<Cv />} />
+        <Route path={PathsMap.cv.get()} element={<Cv />} />
         <Route path={PathsMap.portfolio.get()} element={<Portfolio />} />
         <Route path={PathsMap.blog.get()} element={<Blog />} />
         <Route path={PathsMap.contact.get()} element={<Contact />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
-    </>
+    </main>
   )
 }
 export default MainRouter
