1717f1ece5073753192c8a763ab7f8090b0f9584
mavesna <sungat@mybuh.kz>
2022-09-09 12:38:45 +0600

    fix: add new lib to scroll

--- src/views/aside/Aside.tsx
+++ src/views/aside/Aside.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Profile from '../../components/Simple/aside/Profile'
 import About from '../../components/Simple/aside/About'
-import SoftSkills, { ExtraSkills, HardSkills } from '../../components/Simple/aside/Skills'
-import { Button } from '../../components/UI/buttons'
-import { DownloadIcon } from '../../assets/icons/Icons'
+import SoftSkills, {ExtraSkills, HardSkills} from '../../components/Simple/aside/Skills'
+import {Button} from '../../components/UI/buttons'
+import {DownloadIcon} from '../../assets/icons/Icons'
+import {Scrollbars} from 'react-custom-scrollbars-2'
 
 const Aside = () => {
   return (
     <aside className=" h-screen w-[305px] bg-darkView1 px-10">
       <Profile />
-      <div className="hideScroll mb-6 h-[calc(100vh-450px)] overflow-x-hidden overflow-y-scroll">
+      {/* <div className="hideScroll mb-6 h-[calc(100vh-450px)] overflow-x-hidden overflow-y-scroll"> */}
+      <Scrollbars style={{height: 300}}>
         <hr className="h-px w-full border-none bg-darkView2" />
         <About />
         <hr className="h-px w-full border-none bg-darkView2" />
@@ -19,7 +21,9 @@ const Aside = () => {
         <hr className="h-px w-full border-none bg-darkView2" />
         <ExtraSkills />
         <hr className="h-px w-full border-none bg-darkView2" />
-      </div>
+      </Scrollbars>
+
+      {/* </div> */}
 
       <Button>
         Download cv
