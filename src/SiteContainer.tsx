1404497c41076b429d4ebf8b579abd464e16e44b
qyrBalasy <shakarimsungat@gmail.com>
2022-09-16 14:36:58 +0600

    fix: bg-color

--- src/SiteContainer.tsx
+++ src/SiteContainer.tsx
@@ -3,15 +3,12 @@ import MainRouter from './routes/MainRouter'
 import Aside from './views/aside/Aside'
 import Sidebar from './views/sidebar/Sidebar'
 
-
 const SiteContainer = () => {
   return (
-    <div className="bg-[#18191A]">
-      <div className="container mx-auto flex justify-between gap-x-7">
-        <Aside />
-        <MainRouter />
-        <Sidebar />
-      </div>
+    <div className="container mx-auto flex justify-between gap-x-7">
+      <Aside />
+      <MainRouter />
+      <Sidebar />
     </div>
   )
 }
