9a9659a68e4d2772742bcb748a4c1299c603cfcc
qyrBalasy <shakarimsungat@gmail.com>
2022-09-22 22:45:08 +0600

    feat: add maps on footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { CopyrightIcon } from "../assets/icons/Icons";
+import MyLocation from './Simple/footer/MyLocation'
 
 const Footer = () => {
   return (
-    <footer className="gap-4 bg-darkView1 flex justify-center items-center py-5">
-      <CopyrightIcon className="h-5 w-5" />
-      <p className="text-center text-whiteFont"> 2022 All Rights Reserved. Created by Sungat Shakarim</p>
+    <footer>
+      <MyLocation />
+      <div className="flex items-center justify-center gap-4 bg-darkView1 py-5">
+        <CopyrightIcon className="h-5 w-5" />
+        <p className="text-center text-whiteFont"> 2022 All Rights Reserved. Created by Sungat Shakarim</p>
+      </div>
     </footer>
   )
 }
