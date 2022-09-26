0dc7877892783a37e5b395627959b591590dbe9c
qyrBalasy <shakarimsungat@gmail.com>
2022-09-26 18:30:04 +0600

    feat: add contactForm component

---
+++ src/components/Simple/footer/ContactForm.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <form>
+      <div>
+        <h4>
+          Contact form is here!!!
+        </h4>
+      </div>
+
+    </form>
+  )
+}
+export default ContactForm
