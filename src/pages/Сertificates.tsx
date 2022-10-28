cab970cdb3c9bd549be3aef5fd8d21030843d13a
qyrBalasy <shakarimsungat@gmail.com>
2022-10-28 16:05:51 +0600

    chore: add newicons

--- src/pages/Сertificates.tsx
+++ src/pages/Сertificates.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { JavaScriptIcon, ReactIcon, TypeScriptIcon } from "../assets/icons/Icons";
+import CountDownTimer from "../components/CountDownTimer";
+import MyTimer from "../components/MyTimer";
 import { Button } from "../components/UI/buttons";
 import Cards from "../components/UI/cards";
 
@@ -8,43 +11,41 @@ const Certificates = () => {
       <div className="flex flex-col gap-y-12 ">
         <div className="mx-auto w-96 text-center ">
           <h2 className="text-headline3XL dark:text-whiteFont">Сертификаты</h2>
-          <p className="mt-6">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim
-            velit mollit. lorem ipsum
-          </p>
         </div>
         <div className="grid grid-cols-3 gap-5">
           <Cards className="bg-white dark:bg-darkView1">
             <div>
-              <p>image is here</p>
-              <h4 className="text-headlineLG dark:text-whiteFont">title</h4>
+              <ReactIcon className="w-12" />
+              <h4 className="text-headlineLG dark:text-whiteFont">React+JS</h4>
             </div>
             <div>
-              <p className="text-lightDark dark:text-whiteFont">description</p>
+              <p className="text-lightDark dark:text-whiteFont">React, Hooks, Redux</p>
               <Button color="secondary">перейти</Button>
             </div>
           </Cards>
           <Cards className="bg-white dark:bg-darkView1">
             <div>
-              <p>image is here</p>
-              <h4 className="text-headlineLG dark:text-whiteFont">title</h4>
+              <JavaScriptIcon className="w-12" />
+              <h4 className="text-headlineLG dark:text-whiteFont">Advansed JS</h4>
             </div>
             <div>
-              <p className="text-lightDark dark:text-whiteFont">description</p>
+              <p className="text-lightDark dark:text-whiteFont">Testing, OOP, Module Bundlers</p>
               <Button color="secondary">перейти</Button>
             </div>
           </Cards>
           <Cards className="bg-white dark:bg-darkView1">
             <div>
-              <p>image is here</p>
-              <h4 className="text-headlineLG dark:text-whiteFont">title</h4>
+              <TypeScriptIcon className="w-12" />
+              <h4 className="text-headlineLG dark:text-whiteFont">TypeScript</h4>
             </div>
             <div>
-              <p className="text-lightDark dark:text-whiteFont">description</p>
+              <p className="text-lightDark dark:text-whiteFont">TypeScript basics</p>
               <Button color="secondary">перейти</Button>
             </div>
           </Cards>
         </div>
+        <CountDownTimer />
+        <MyTimer time={1} />
       </div>
     </section>
   )
