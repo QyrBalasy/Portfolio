5d366262cba8abdb4bec310a31df2b43a5bc1001
qyrBalasy <shakarimsungat@gmail.com>
2022-10-31 16:27:22 +0600

    feat: add deadline timer

--- src/pages/Сertificates.tsx
+++ src/pages/Сertificates.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { JavaScriptIcon, ReactIcon, TypeScriptIcon } from "../assets/icons/Icons";
-import CountDownTimer from "../components/CountDownTimer";
-import MyTimer from "../components/MyTimer";
+import DeadlineTimer from "../components/Other/DeadlineTimer";
 import { Button } from "../components/UI/buttons";
 import Cards from "../components/UI/cards";
 
@@ -20,7 +19,10 @@ const Certificates = () => {
             </div>
             <div>
               <p className="text-lightDark dark:text-whiteFont">React, Hooks, Redux</p>
-              <Button color="secondary">перейти</Button>
+              <Button color="secondary" disabled={true}>
+                перейти
+              </Button>
+              <DeadlineTimer finalDate="2022-12-20T00:00:00" />
             </div>
           </Cards>
           <Cards className="bg-white dark:bg-darkView1">
@@ -30,7 +32,10 @@ const Certificates = () => {
             </div>
             <div>
               <p className="text-lightDark dark:text-whiteFont">Testing, OOP, Module Bundlers</p>
-              <Button color="secondary">перейти</Button>
+              <Button color="secondary" disabled={true}>
+                перейти
+              </Button>
+              <DeadlineTimer finalDate="2023-01-01T00:00:00" />
             </div>
           </Cards>
           <Cards className="bg-white dark:bg-darkView1">
@@ -40,12 +45,13 @@ const Certificates = () => {
             </div>
             <div>
               <p className="text-lightDark dark:text-whiteFont">TypeScript basics</p>
-              <Button color="secondary">перейти</Button>
+              <Button color="secondary" disabled={true}>
+                перейти
+              </Button>
+              <DeadlineTimer finalDate="2023-02-01T00:00:00" />
             </div>
           </Cards>
         </div>
-        <CountDownTimer />
-        <MyTimer time={1} />
       </div>
     </section>
   )
